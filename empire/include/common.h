744f8aebd25eb1bc112f7aa9feb5b7505901c83e
Nianavy <gugeemail@qq.com>
2025-08-29 17:39:58 +0800

    add clientstate-struct

--- empire/include/common.h
+++ empire/include/common.h
@@ -4,4 +4,16 @@
 #define STATUS_ERROR    -1
 #define STATUS_SUCCESS  0
 
+typedef enum {
+    STATE_NEW,
+    STATE_CONNECTED,
+    STATE_DISCONNECTED
+} state_e;
+
+typedef struct {
+    int fd;
+    state_e state;
+    char buffer[4096];
+} clientstate_t;
+
 #endif
