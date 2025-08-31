6dd63054a4ceb07dac98c91bfaa24a051e905b9e
Nianavy <gugeemail@qq.com>
2025-09-01 07:38:59 +0800

    feat(IPC): 共享内存通信

---
+++ demo/multi-process/02-Inter-Process-Communication-ShareMemory.c
@@ -0,0 +1,63 @@
+/*
+demo for IPC. using share memory.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+
+#define SHM_NAME "/my_shared_memory"
+#define SHM_SIZE 1024
+
+int main() {
+    int shm_fd;
+    char *shm_ptr;
+
+    shm_fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (shm_fd == 01) {
+        perror("shm_open failed");
+        exit(1);
+    }
+
+    if (ftruncate(shm_fd, SHM_SIZE) == -1) {
+        perror("ftruncate failed");
+        exit(1);
+    }
+
+    shm_ptr = (char *)mmap(NULL, SHM_SIZE, PROT_READ | PROT_WRITE,
+                        MAP_SHARED, shm_fd, 0);
+    if (shm_ptr == MAP_FAILED) {
+        perror("mmap failed");
+        exit(1);
+    }
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        exit(1);
+    }
+    else if (pid == 0) {
+        strcpy(shm_ptr, "Ciao from sub-process!");
+        printf("sub-process write in: %s\n", shm_ptr);
+
+        munmap(shm_ptr, SHM_SIZE);
+        close(shm_fd);
+    }
+    else {
+        sleep(1);
+        printf("par-process read out: %s\n", shm_ptr);
+
+        wait(NULL);
+
+        munmap(shm_ptr, SHM_SIZE);
+        close(shm_fd);
+        shm_unlink(SHM_NAME);
+    }
+
+    return 0;
+}
